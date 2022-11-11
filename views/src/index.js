f23eefd6ce1edbeb1dba227c3e052ff59f565a75
Alexander Podstrechnyy <tankalxat34@gmail.com>
2022-11-11 20:13:46 +0300

    Мелкие правки

--- views/src/index.js
+++ views/src/index.js
@@ -1,1 +1,8 @@
-console.log("Программа загружена!")
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     let user_url = new URL(window.location.href)
+//     if (window.location.href.includes("?")) {
+//         user_url.searchParams.delete("access_token")
+//         window.location.href = user_url.href
+//     }
+// })
