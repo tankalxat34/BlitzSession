9f0ff7b7ff8c1f3c83fcc9e1acdf4d453cd71097
Alexander Podstrechnyy <tankalxat34@gmail.com>
2022-11-15 10:04:04 +0300

    Подготовил запросы без авторизации пользователя

--- index.js
+++ index.js
@@ -12,18 +12,45 @@ app.set('view engine', 'ejs');
 const PORT = process.env.PORT || 3000
 const LESTA_APP_ID = process.env.LESTA_APP_ID
 
+// https init
+const https = require('https')
+
 // set up default path to content
 app.use(express.static(`${__dirname}/views`));
 
 app.get('/auth', (req, res) => {
-    res.redirect(`https://api.tanki.su/wot/auth/login/?application_id=${LESTA_APP_ID}&redirect_uri=http://localhost:${PORT}`)
+    res.redirect(`https://api.tanki.su/wot/auth/login/?application_id=${LESTA_APP_ID}&redirect_uri=http://localhost:${PORT}/auth_back`)
+})
+
+app.get("/auth_back", (req, res) => {
+    res.redirect("/", {
+        lesta_auth: req.query
+    })
+})
+
+app.get("/stat", (req, res) => {
+    https.get(`https://api.wotblitz.ru/wotb/account/info/?application_id=${LESTA_APP_ID}&account_id=${req.query.id}`, (resp) => {
+        // res.render("stat", { lesta: { nickname: "nkatnt" } })
+        // console.log(resp)
+        let data = '';
+
+        // A chunk of data has been received.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        resp.on('end', () => {
+            let parsedData = JSON.parse(data);
+            // res.send(data)
+            res.render("stat", {lesta: parsedData.data[req.query.id]})
+        });
+    })
 })
 
 app.get('/', (req, res) => {
-    lesta_auth = req.query
-    res.render("full_stat", {
-        lesta_auth: lesta_auth
-    })
+    console.log(req)
+    res.render("full_stat", { lesta_auth: {} })
+    // res.render("full_stat", {lesta_auth: })
 })
 
 app.listen(PORT, () => {
